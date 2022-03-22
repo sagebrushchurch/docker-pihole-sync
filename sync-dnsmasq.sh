8fba56252f9018d9e634b11ff5afaaa6bbf12466
Jon Krake <1024jon@gmail.com>
2022-03-22 16:44:14 -0600

    fixes sync issues, at least for my volume setup or something

--- sync-dnsmasq.sh
+++ sync-dnsmasq.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-inotifywait -r -m -e close_write --exclude '01-pihole\.conf' --format '%w%f' /mnt/etc-dnsmasq.d/ | while read MODFILE
+inotifywait -r -m -e modify --exclude '01-pihole\.conf' --format '%w%f' /mnt/etc-dnsmasq.d/ | while read MODFILE
 do
     rsync -a -P --exclude '01-pihole.conf' /mnt/etc-dnsmasq.d/ -e "ssh -p ${REM_SSH_PORT}" root@${REM_HOST}:/mnt/etc-dnsmasq.d/ --delete
     if [[ "${?}" -ne "0" ]]; then
